bb0be9c476edd717125557a5a876c3f9516d8e75
DESKTOP-95BTHUM\dndud <dndud1997@naver.com>
2020-07-30 21:55:40 +0900

    07/30 관리자 페이지 폴더 정리

---
+++ src/main/java/com/web/bomulsum/manager/controller/managerController.java
@@ -0,0 +1,14 @@
+package com.web.bomulsum.manager.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class managerController {
+
+	@GetMapping("/home")
+	public String home() {
+		return "home";
+	}
+	
+}
