12f32268fa3c8f046db5635107fe933459523a4b
KNuri <kmw6078@gmail.com>
2020-08-06 20:54:49 +0900

    [강민우] 관리자쪽 게시판 기능 구현 (CRUD 완료) + web.xml 인코딩필터 추가

--- src/main/java/com/web/bomulsum/manager/board/repository/MBoardDAO.java
+++ src/main/java/com/web/bomulsum/manager/board/repository/MBoardDAO.java
@@ -12,12 +12,31 @@ public class MBoardDAO {
 	@Autowired
 	private SqlSessionTemplate sqlSessionTemplate;
 	
+	//글 등록
 	public void mboardRegister(MBoardVO vo) {
 		sqlSessionTemplate.insert("mBoardDAO.insertMBoard", vo);
 	}
 	
+	//게시판 글 목록 불러오기
 	public List<MBoardListVO> mboardList(){
 		return sqlSessionTemplate.selectList("mBoardDAO.selectMBoard");
 	}
 	
+	//글 수정을 위한 데이터 불러와서 수정폼에 뿌려주기
+	public MBoardVO mboardSelectOne(MBoardVO vo) {
+		MBoardVO result = sqlSessionTemplate.selectOne("mBoardDAO.selectRewrite", vo);
+		return result;
+	}
+	
+	//글 수정 반영
+	public void mboardUpdate(MBoardVO vo) {
+		System.out.println("업데이트 입력될 데이터 : " + vo.toString());
+		sqlSessionTemplate.update("mBoardDAO.updateMBoard", vo);
+	}
+	
+	
+	public void mboardDelete(MBoardVO vo) {
+		System.out.println("삭제될 데이터 시퀀스 : "+ vo.getAdminNoticeSeq());
+		sqlSessionTemplate.delete("mBoardDAO.deleteMBoard", vo);
+	}
 }
