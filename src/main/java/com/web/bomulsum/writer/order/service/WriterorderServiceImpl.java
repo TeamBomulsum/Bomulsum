61b00616e8271ac485b0809e2905714fc5388270
KNuri <kmw6078@gmail.com>
2020-09-23 19:27:49 +0900

    강민- 백업용 커밋

---
+++ src/main/java/com/web/bomulsum/writer/order/service/WriterorderServiceImpl.java
@@ -0,0 +1,8 @@
+package com.web.bomulsum.writer.order.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class WriterorderServiceImpl implements WriterOrderService {
+
+}
